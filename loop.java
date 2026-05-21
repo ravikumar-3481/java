28007c7716a3c168965ed56cef2a522524ea05e5
Ravi <ravivish968@gmail.com>
2026-05-21 20:16:14 +0530

    update loop.java

--- loop.java
+++ loop.java
@@ -25,35 +25,40 @@ public class loop {
         printStarPatternReverseRec(n - 1);
     }
 
-    public static void takeInputData() {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter Name : ");
-            String name = sc.nextLine();
-            System.out.print("Enter Age : ");
-            int age = sc.nextInt();
-            System.out.println("Your name is " + name + " and your age is " + age);
-        }
+    public static void takeInputData(Scanner sc) {
+        System.out.print("Enter Name : ");
+        String name = sc.nextLine();
+        System.out.print("Enter Age : ");
+        int age = sc.nextInt();
+        System.out.println("Your name is " + name + " and your age is " + age);
     }
 
-    public static void calc(int a , int b) {
+    public static void calc(Scanner sc1) {
+       
+        System.out.print("Enter the value of a :  ");
+        int a = sc1.nextInt();
+        System.out.print("Enter the value of b :  ");
+        int b = sc1.nextInt();
         int sum = a + b;
         int diff = a - b;
         int product = a * b;
-        int quotient = a / b;
+        float quotient = (float) a / b;
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + diff);
         System.out.println("Product: " + product);
-        System.out.println("Quotient: " + quotient);
+        System.out.printf("Quotient: %.2f%n", quotient);
+        sc1.close();
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = 5; // You can change this value to print a different pattern
         printPattern(n);
         System.out.println(); // Print a blank line
         printStarPatternReverse(n);
         System.out.println();
-        takeInputData();
-        calc(10, 5);
-        
+        takeInputData(sc);
+        calc(sc);
+
     }
 }
